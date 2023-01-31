cf9bb9de066d14881f52d686cebe2b9c57870eb8
AnnaMC00 <am.casariegoc00@gmail.com>
2023-01-31 00:16:40 -0600

    Chamber, viewports added.

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -14,4 +14,4 @@ document.querySelector("#date").innerText = completeDate;
 const lastModif = new Date(document.lastModified);
 const lastDate = new Intl.DateTimeFormat("en-US", { year: "numeric", month: "2-digit", day: "2-digit" }).format(lastModif);
 const lastTime = new Intl.DateTimeFormat("en", { timeStyle: "medium", hour12: false }).format(lastModif);
-document.querySelector("#last-update").innerText = `Last Updated: ${lastDate} ${lastTime}`;
+document.querySelector("#last-update").innerText = `${lastDate} ${lastTime}`;
