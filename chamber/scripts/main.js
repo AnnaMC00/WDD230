0a9006cab1b4eadbf1cdfdb11d958229555cc8f6
AnnaMC00 <am.casariegoc00@gmail.com>
2023-03-11 02:52:10 -0600

    Directory Page Added

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -66,26 +66,90 @@ const visitinfo = document.querySelector("#visitinfo");
 
 let lastvisit = Number(window.localStorage.getItem("last-visit"));
 
-if (lastvisit !== 0) {
-    let daysmiliseconds = Date.now() - lastvisit;
-    let days = (daysmiliseconds / 84600000).toFixed(0);
+if (visitinfo) {
+    if (lastvisit !== 0) {
+        let daysmiliseconds = Date.now() - lastvisit;
+        let days = (daysmiliseconds / 84600000).toFixed(0);
 
-    if (days == 1) {
-        visitinfo.textContent = `Your last visit was ${days} day ago! Welcome again!`;
-    }
-    else if (days == 0) {
-        visitinfo.textContent = "Your last visit was today!"
+        if (days == 1) {
+            visitinfo.textContent = `Your last visit was ${days} day ago! Welcome again!`;
+        }
+        else if (days == 0) {
+            visitinfo.textContent = "Your last visit was today!"
+        }
+        else {
+            visitinfo.textContent = `Your last visit was ${days} days ago! Welcome again!`;
+        }
     }
     else {
-        visitinfo.textContent = `Your last visit was ${days} days ago! Welcome again!`;
+        visitinfo.textContent = "This is your first visit! Welcome!";
     }
-}
-else {
-    visitinfo.textContent = "This is your first visit! Welcome!";
 }
 
 localStorage.setItem("last-visit", Date.now())
 
+
+// DIRECTORY PAGE
+const directoryButton = document.querySelector("#directory-button");
+const article = document.querySelector("#directory-display");
+
+const alternateGridList = () => {
+    let className = article.className;
+    if (className == "directory-grid") {
+        article.classList.add("directory-list");
+        article.classList.remove("directory-grid");
+        directoryButton.textContent = "Grid";
+    }
+    else if (className == "directory-list") {
+        article.classList.add("directory-grid");
+        article.classList.remove("directory-list");
+        directoryButton.textContent = "List";
+    }
+};
+
+directoryButton.addEventListener("click", alternateGridList);
+
+const url = "https://annamc00.github.io/wdd230/chamber/scripts/data.json";
+
+async function getDirectoryData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayDirectory(data);
+}
+
+getDirectoryData();
+
+const displayDirectory = (businesses) => {
+    const article = document.querySelector("#directory-display");
+    businesses.forEach((business) => {
+        let card = document.createElement("section");
+        let h3 = document.createElement("h3");
+        let icon = document.createElement("img");
+        let address = document.createElement("p");
+        let phone = document.createElement("a");
+        let url = document.createElement("a");
+        let level = document.createAttribute("p");
+
+        h3.textContent = business.name;
+        icon.setAttribute("src", business.icon);
+        icon.setAttribute("alt", `${business.name} icon.`);
+        icon.setAttribute("loading", "lazy");
+        address.textContent = business.address;
+        phone.setAttribute("href", `tel:${business.phone}`);
+        phone.textContent = business.phone;
+        url.setAttribute("href", business.url);
+        url.textContent = business.url;
+        level.textContent = business.membership - level;
+
+        card.appendChild(h3);
+        card.appendChild(icon);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+
+        article.appendChild(card);
+    });
+}
 
 // -------------------- FOOTER ----------------------
 const year = date.getFullYear();
