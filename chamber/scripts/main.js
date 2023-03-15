17d142b707c3ec2a399d58b88a2ae7492aeff287
AnnaMC00 <am.casariegoc00@gmail.com>
2023-03-14 21:34:15 -0500

    bugs fixed

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -61,33 +61,6 @@ else {
     });
 }
 
-// Visits
-const visitinfo = document.querySelector("#visitinfo");
-
-let lastvisit = Number(window.localStorage.getItem("last-visit"));
-
-if (visitinfo) {
-    if (lastvisit !== 0) {
-        let daysmiliseconds = Date.now() - lastvisit;
-        let days = (daysmiliseconds / 84600000).toFixed(0);
-
-        if (days == 1) {
-            visitinfo.textContent = `Your last visit was ${days} day ago! Welcome again!`;
-        }
-        else if (days == 0) {
-            visitinfo.textContent = "Your last visit was today!"
-        }
-        else {
-            visitinfo.textContent = `Your last visit was ${days} days ago! Welcome again!`;
-        }
-    }
-    else {
-        visitinfo.textContent = "This is your first visit! Welcome!";
-    }
-}
-
-localStorage.setItem("last-visit", Date.now())
-
 
 // DIRECTORY PAGE
 const directoryButton = document.querySelector("#directory-button");
