1db3b51362bdd277a11064f76ab9e45e9729b65a
AnnaMC00 <am.casariegoc00@gmail.com>
2023-03-14 20:01:59 -0500

    Home Page Weather Section updated

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,55 @@
-const temp = parseInt(document.querySelector("#grades").textContent);
-const windspeed = parseInt(document.querySelector("#wind-speed").textContent);
-const windchill = document.querySelector("#wind-chill");
+const urlweather = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&units=imperial&appid=3c41b73bcb169357363e3d2f07586d38";
 
-if (temp <= 50 && windspeed > 3) {
-    let f = 35.74 + 0.6215 * temp - 35.75 * windspeed ** 0.16 + 0.4275 * temp * windspeed ** 0.16;
-    f = f.toFixed(1);
-    windchill.textContent = `${f}°F`;
+async function apiFetch() {
+    try {
+        const response = await fetch(urlweather);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            setValues(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
-else {
-    console.log(windchill);
+
+apiFetch();
+
+const temp = document.querySelector("#grades");
+const windspeed = document.querySelector("#wind-speed");
+const windchill = document.querySelector("#wind-chill");
+const description = document.querySelector("#weather-description");
+const icon = document.querySelector("#weather-icon");
+
+function setValues(weatherdata) {
+    const iconurl = `https://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`;
+    let desc = weatherdata.weather[0].description;
+    desc = desc.split(" ");
+
+    for (let i = 0; i < desc.length; i++) {
+        desc[i] = desc[i][0].toUpperCase() + desc[i].substr(1);
+    }
+    desc = desc.join(" ");
+
+    const t = weatherdata.main.temp.toFixed();
+    const ws = weatherdata.wind.speed;
+
+    if (t <= 50 && ws > 3) {
+        let f = 35.74 + 0.6215 * t - 35.75 * ws ** 0.16 + 0.4275 * t * ws ** 0.16;
+        f = f.toFixed(1);
+        windchill.textContent = `${f}°F`;
+    }
+    else {
+        windchill.textContent = "N/A";
+    }
+
+    temp.textContent = t;
+    description.innerHTML = `<b>${desc}</b>`;
+    windspeed.textContent = ws;
+    icon.setAttribute("src", iconurl);
+    icon.setAttribute("alt", desc);
 }
