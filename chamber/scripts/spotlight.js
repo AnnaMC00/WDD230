6913c40a26d7b02cfc7860bd362b13ae7f0c2d15
AnnaMC00 <am.casariegoc00@gmail.com>
2023-03-18 23:50:20 -0500

    Spotlight JS added

---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,43 @@
+const gold1 = document.querySelector("#gold1");
+const gold2 = document.querySelector("#gold2");
+const silver = document.querySelector("#silver");
+
+const url = "https://annamc00.github.io/wdd230/chamber/scripts/data.json";
+
+async function getDirectoryData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    constructSpotlights(data);
+}
+
+getDirectoryData();
+
+const randomBusiness = (businesses, level) => {
+    const results = businesses.filter(business => business.membership == level);
+    return results;
+}
+
+const constructSpotlights = (businesses) => {
+    const gold = randomBusiness(businesses, "Gold");
+    console.log(gold);
+
+    gold1.innerHTML = `<h3>${gold[0].name}</h3>
+                    <p>${gold[0].description}</p>
+                    <hr>
+                    <a href="#" class="info">${gold[0].mail}</a>
+                    <a href="#" class="info">${gold[0].url}</a>`;
+
+    gold1.innerHTML = `<h3>${gold[1].name}</h3>
+                    <p>${gold[1].description}</p>
+                    <hr>
+                    <a href="#" class="info">${gold[1].mail}</a>
+                    <a href="#" class="info">${gold[1].url}</a>`;
+
+    const silver = randomBusiness(businesses, "Silver");
+
+    gold1.innerHTML = `<h3>${silver[0].name}</h3>
+                    <p>${silver[0].description}</p>
+                    <hr>
+                    <a href="#" class="info">${silver[0].mail}</a>
+                    <a href="#" class="info">${silver[0].url}</a>`;
+} 
